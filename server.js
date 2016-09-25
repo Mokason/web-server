716101500b8fff78057a0d1ea27e5505a7408a0b
Arturs Rozens <arturs2952@gmail.com>
2016-09-25 23:12:22 +0300

    Refactor middleware

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
-//about
-app.get('/about', function(req, res){
+
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
 	res.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(PORT, function(){
-	console.log('Express server started on port :' + PORT +'!');
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT + '!');
 });
